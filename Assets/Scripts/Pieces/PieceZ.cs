c8eec12809bdbd8e85c2af854e24c547fc300991
Diena <dienapinochka@gmail.com>
2024-11-08 20:24:42 +0300

    PieceZ.cs have been codestyled

--- Assets/Scripts/Pieces/PieceZ.cs
+++ Assets/Scripts/Pieces/PieceZ.cs
@@ -4,10 +4,7 @@ namespace Pieces
 {
     public class PieceZ : Piece
     {
-        private readonly Vector2Int[] _shape =
-        {
-            new Vector2Int(-1, 1), new Vector2Int(0, 1), new Vector2Int(0, 0), new Vector2Int(1, 0)
-        };
+        private readonly Vector2Int[] _shape = {new(-1, 1), new(0, 1), new(0, 0), new(1, 0)};
 
         protected override Vector2Int[] ShapeMap()
         {
