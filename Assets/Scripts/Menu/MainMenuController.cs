1978024354c72dbf444888800a0975196b6a87d6
Diena <dienapinochka@gmail.com>
2024-12-21 14:49:11 +0300

    Continue button was created

--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -2,12 +2,41 @@
 using Board.Data;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Menu
 {
     public class MainMenuController : MonoBehaviour
     {
-        public void StartGame()
+        [SerializeField] private GameObject continueButtonPrefab;
+        [SerializeField] private Transform canvasTransform;
+
+        private void Start()
+        {
+            // if (TetrisInfo.Instance.TetrisStage() != ETetrisStage.OnPauseMenu) return;
+            if (continueButtonPrefab == null || canvasTransform == null)
+            {
+                Debug.LogError("Button prefab or canvasTransform not set!");
+                return;
+            }
+
+            GameObject newButton = Instantiate(continueButtonPrefab, canvasTransform);
+            Button buttonComponent = newButton.GetComponent<Button>();
+
+            if (buttonComponent == null) Debug.LogError("Button component not found on prefab!");
+
+            buttonComponent.onClick.RemoveAllListeners();
+            buttonComponent.onClick.AddListener(Continue);
+        }
+
+        public void StartNewGame()
+        {
+            SceneManager.LoadScene("Tetris");
+            EventsHub.OnStageChanged.Trigger(ETetrisStage.OnGame);
+            Debug.Log("Scene 'Tetris' loaded");
+        }
+
+        public void Continue()
         {
             SceneManager.LoadScene("Tetris");
             EventsHub.OnStageChanged.Trigger(ETetrisStage.OnGame);
@@ -27,6 +56,7 @@ namespace Menu
 
         private void Update()
         {
+            // if (TetrisInfo.Instance.TetrisStage() != ETetrisStage.OnPauseMenu) return;
             if (!Input.GetKeyDown(KeyMap.KeyContinue)) return;
             Debug.Log($"[KEY PRESS] {KeyMap.KeyContinue.ToString()}");
             EventsHub.OnStageChanged.Trigger(ETetrisStage.OnGame);
